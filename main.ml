b411a0015b147409374ecf1e0c2ad1de6daeceb2
Yusuke Izawa <me@yizawa.com>
2024-11-22 20:13:11 +0900

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,6 +1,7 @@
 open Syntax
 
-(* 入力文字列が構文木に変換できるか確かめる*)  
+(* 課題1,2,3 *)
+(* 入力文字列が構文木に変換できるか確かめる*)
 let test_syntax s =
   print_string "[TEST syntax]\n";
   print_string "input: ";
@@ -11,34 +12,41 @@ let test_syntax s =
   |> Syntax.string_of_statement
   |> print_string
 
-(* 入力文字列が仮想スタック命令に変換できるか確かめる*)  
+
+(* 課題1,2,3 *)
+(* 入力文字列 (predicate) が構文木に変換できるか確かめる*)
+let test_syntax_predicate p =
+  print_string "[TEST predicate]\n";
+  print_string "input: ";
+  print_endline p;
+  print_string "output: ";
+  let l = Lexing.from_string p in
+  Parser.predicate Lexer.token l
+  |> Syntax.string_of_predicate
+  |> print_string; print_newline()
+
+
+(* 課題4 *)
+(* 入力文字列が仮想スタック機械命令列に変換できるか確かめる*)
 let test_stack_ops s =
   print_string "[TEST stack ops]\n";
   print_string "input: ";
   print_endline s;
-  print_endline "output: ";
+  print_string "output: ";
   let l = Lexing.from_string s in
   Parser.start Lexer.token l
-  |> Virtual_stack.compile
+  |> Virtual_stack.compile_stack
   |> Virtual_stack.print_code stdout
 
-(* predicate の構文木を自分で定義し、実際に文字列に変換できるか確かめる *)
-let test_syntax_predicate _ =  
-  print_string "[TEST predicate]\n";
-  let p = LT(Var("i"), Num(2)) in (* i < 2 *)
-  print_string (Syntax.string_of_predicate p);
-  print_newline ();
-  let p = LT (Var ("i"), Add (Var ("i"), Num (2))) in
-  print_string (Syntax.string_of_predicate p);
-  print_newline ();
-  ()
 
 let () =
     test_syntax "i := i + 1;";
-    test_syntax "i := i - 1;";
+    (* test_syntax "i := i - 1;"; *)
     (* test_syntax "i := i * 1;"; *)
-    test_syntax_predicate ();
-    test_stack_ops "i := i + 1;";
+    test_syntax_predicate "i < 10";
+    (* test_syntax_predicate "i > 10"; *)
+    (* test_stack_ops "i := i + 1;"; *)
     (* test_stack_ops "i := i - 1;"; *)
     (* test_stack_ops "i := i * 1;"; *)
     (* test_stack_ops "i := i / 1;"; *)
+    ()
